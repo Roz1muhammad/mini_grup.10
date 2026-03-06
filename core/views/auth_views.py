21ebf1774df4a2cf7b97585196826bdca39a29da
AbduvorisFirst <abduvoris2bbc@gmail.com>
2026-03-06 16:49:43 +0500

    hozir biz register 50 procent qildim Abduvoris
    boldi shu

--- core/views/auth_views.py
+++ core/views/auth_views.py
@@ -1,23 +1,31 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from core.models.auth_models import User
+from rest_framework.exceptions import AuthenticationFailed
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        fullname = request.body.get("fullname", None)
-        username = request.body.get("username", None)
-        age = request.body.get('age', None)
-        gender = request.body.get("gender", None)
-        role = request.body.get("role", None)
+        fullname = request.data.get("fullname", None)
+        username = request.data.get("username", None)
+        age = request.data.get('age', None)
+        gender = request.data.get("gender", None)
+        role = request.data.get("role", None)
 
         if None in [fullname, username, age, gender, role]:
             return Response({
                 "error": "kerakli, polyalar to'lliq emas"
             }, status=403)
 
-        # user = User.objects.
+        user = User.objects.filter(username=username).first()
+        if user:
+            raise AuthenticationFailed("user already exist")
+
+        return Response({
+            'message': "hozircha hammasi cho'tki"
+        })
 
 
 
